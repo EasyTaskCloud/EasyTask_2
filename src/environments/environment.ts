bdeb1f4851efdf0e2e7120232b28afd0ab29d84b
Sebastian <93546847+harry05555@users.noreply.github.com>
2025-11-19 23:52:00 +0100

    add environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { EnvironmentInterface } from "./core/environment.interface";
+
+export const environment: EnvironmentInterface = {
+    production: true,
+    apiUrl: "",
+    version: "1.0",
+}
